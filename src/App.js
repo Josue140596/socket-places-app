0cff809a1755f502ed34a44db42c9e96a449f2ac
Josue140596 <josue140596@gmail.com>
2021-02-07 16:09:53 -0600

    spinner

--- src/App.js
+++ src/App.js
@@ -31,14 +31,14 @@ function App() {
       {/* Nav bar */}
       <nav className="navbar nav-con">
         <div className='d-flex'>
-          {/* <img className='m-2 ml-3' src={logo} alt='img' /> */}
+
           <h3 className='m-2  '>Best places to visit</h3>
         </div>
 
         <div className='d-flex'>
-        
+
           {
-            
+
             online
               ? <span className='m-4 text-success'>Online</span>
               : <span className='m-4 text-danger'>Ofline</span>
@@ -55,23 +55,30 @@ function App() {
         <div className="row">
 
           {/* places */}
-          <div className="col ">
-
-            <PlacesList/>
-          </div>
+          {
+            (online)
+            ? <div className="col "><PlacesList /></div>
+              
+            
+            : <div className='loading'>
+            <div className="spinner-border" style={{ "width": '3rem', 'height': "3rem" }} role="status"></div>
+            </div> 
+          }
 
           {/* Grafica */}
-          <div className="col ">
-            <PlaceChart />
+          <div className="col "> 
+          <PlaceChart />
 
             {/* Add place */}
 
             <AddPlace />
           </div>
+
+
         </div>
       </div>
 
-    </div>
+    </div >
   );
 }
 
